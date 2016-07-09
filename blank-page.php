bb1e11aa352ecd6e598d1de51ab0f60567bd0f18
Michael Verdi <Verdi@users.noreply.github.com>
2016-07-09 13:01:11 -0500

    Added <meta name="viewport" content="width=device-width"> to the head

--- blank-page.php
+++ blank-page.php
@@ -1,34 +1,48 @@
 <?php
 /*
  * Template Name: Blank Page
- * Description: This WordPress page template removes all of the special WordPress html from a page so that you can write whatever html you want between the body tags and style it with it's own css file.  
+ * Description: A Page Template with a darker design.
  */
+
+// Code to display Page goes here...
 ?>
 <?php remove_filter ('the_content', 'wpautop'); 
-	// This removes extra html (like <p> tags) that Wordpress sticks into your post
+	// This removes html that Wordpress sticks into your post
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
 <?php $title = get_post_meta($post->ID, 'Title', true);
 if ($title) {
-	// This allows you to use a "Title" custom field to set the page title.
+	// This allows me to use a "Title" custom field to set the page title.
 ?>
 <title><?php echo $title; ?> | <?php bloginfo('name'); ?></title>
 <?php }
 ?>
-<link rel="shortcut icon" type="image/jpg" href="LINK TO YOUR SHORTCUT ICON">
-<link rel="apple-touch-icon-precomposed" href="LINK TO YOUR SHORTCUT ICON">
+<link rel="shortcut icon" type="image/jpg" href="/x.jpg">
+<link rel="apple-touch-icon-precomposed" href="/x.jpg">
 <meta name="mobile-web-app-capable" content="yes" />
-<link rel="alternate" type="application/rss+xml" title="WEBSITE NAME &raquo; Feed" href="http://WEBSITEURL/feed/" />
-<link rel="stylesheet" href="URL OF YOUR CHILD THEME DIRECTORY/reset.css" type="text/css" media="all" /><!-- I use Eric Meyer's http://meyerweb.com/eric/tools/css/reset/reset.css -->
+<link rel="alternate" type="application/rss+xml" title="X-RAY SIX TWO SEVEN &raquo; Feed" href="http://x627.com/feed/" />
+<!--
+
+ __   __            ____    ______   __    __      ____    ______  __   __      ______  __      __  _____       ____    ____    __  __  ____    __  __     
+/\ \ /\ \          /\  _`\ /\  _  \ /\ \  /\ \    /\  _`\ /\__  _\/\ \ /\ \    /\__  _\/\ \  __/\ \/\  __`\    /\  _`\ /\  _`\ /\ \/\ \/\  _`\ /\ \/\ \    
+\ `\`\/'/'         \ \ \L\ \ \ \L\ \\ `\`\\/'/    \ \,\L\_\/_/\ \/\ `\`\/'/'   \/_/\ \/\ \ \/\ \ \ \ \ \/\ \   \ \,\L\_\ \ \L\_\ \ \ \ \ \ \L\_\ \ `\\ \   
+ `\/ > <     _______\ \ ,  /\ \  __ \`\ `\ /'      \/_\__ \  \ \ \ `\/ > <        \ \ \ \ \ \ \ \ \ \ \ \ \ \   \/_\__ \\ \  _\L\ \ \ \ \ \  _\L\ \ , ` \  
+    \/'/\`\ /\______\\ \ \\ \\ \ \/\ \ `\ \ \        /\ \L\ \ \_\ \__ \/'/\`\      \ \ \ \ \ \_/ \_\ \ \ \_\ \    /\ \L\ \ \ \L\ \ \ \_/ \ \ \L\ \ \ \`\ \ 
+    /\_\\ \_\/______/ \ \_\ \_\ \_\ \_\  \ \_\       \ `\____\/\_____\/\_\\ \_\     \ \_\ \ `\___x___/\ \_____\   \ `\____\ \____/\ `\___/\ \____/\ \_\ \_\
+    \/_/ \/_/          \/_/\/ /\/_/\/_/   \/_/        \/_____/\/_____/\/_/ \/_/      \/_/  '\/__//__/  \/_____/    \/_____/\/___/  `\/__/  \/___/  \/_/\/_/
+
+-->
+<link rel="stylesheet" href="http://x627.com/brain/wp-content/themes/twentyfifteen-child/reset.css" type="text/css" media="all" />
 <?php $stylesheet = get_post_meta($post->ID, 'Stylesheet', true);
 if ($stylesheet) {
 ?>
-<link rel="stylesheet" href="URL OF YOUR CHILD THEME DIRECTORY<?php echo $stylesheet; ?>.css" type="text/css" media="screen,projection,tv" />
+<link rel="stylesheet" href="http://x627.com/brain/wp-content/themes/twentyfifteen-child/<?php echo $stylesheet; ?>.css" type="text/css" media="screen,projection,tv" />
 <?php }
-	//This allows you to use a "Stylesheet" custom field to link to a style sheet in the child them directory.
+	//This allows me to use a "Stylesheet" custom field to link to a style sheet in the child them directory.
 ?>
 </head>
 
